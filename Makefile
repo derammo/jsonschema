a967b411b57b43a00feb885f98ca88d4f11a911e
derammo <817160+derammo@users.noreply.github.com>
2021-11-10 22:11:51 -0500

    build files now to be supplied by host project

--- Makefile
+++
@@ -1,3 +0,0 @@
-jar:
-	jar -cvf jsonschema-1.0.1.jar LICENSE NOTICE
-	cd bin && jar -uvf ../jsonschema-1.0.1.jar net
--- src/main/java/net/derammo/jsonschema/SchemaDraft3.java
+++ src/main/java/net/derammo/jsonschema/SchemaDraft3.java
@@ -19,7 +19,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 
 import net.derammo.jsonschema.TypeDeserializer.TypeVariant;
 
